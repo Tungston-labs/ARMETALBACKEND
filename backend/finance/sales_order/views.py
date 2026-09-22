271a4962fa73d92ef6d29ee82ef9dac9c22291a9
Tungston-labs <support@tungstonlabs.com>
2026-09-22 12:45:00 +0530

    added import

--- backend/finance/sales_order/views.py
+++ backend/finance/sales_order/views.py
@@ -198,7 +198,7 @@ from django.db.models import ProtectedError
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
+from finance.invoice.models import Invoice
 class SalesOrderDetailView(
     generics.RetrieveUpdateDestroyAPIView
 ):
